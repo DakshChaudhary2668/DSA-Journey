d86b40e15fc8d8730f91921f492d63243701334b
Daksh Chaudhary <dakshchaudhary2668@gmail.com>
2026-06-22 23:24:41 +0530

    Time: 1 ms (75.64%), Space: 60.6 MB (39.64%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,19 @@
+var moveZeroes = function(nums) {
+
+    let j = 0;
+
+    for(let i = 0; i < nums.length; i++){
+
+        if(nums[i] !== 0){
+            nums[j] = nums[i];
+            j++;
+        }
+
+    }
+
+    while(j < nums.length){
+        nums[j] = 0;
+        j++;
+    }
+
+};
